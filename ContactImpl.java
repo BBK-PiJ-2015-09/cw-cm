f4674a35c2eb487b4c44d31fb15229d12701c4ef
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-05 20:00:57 +0000

    Add comments to ContactImpl

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,6 +1,12 @@
 public class ContactImpl implements Contact {
 	private int id;
 
+	/**
+	 * Constructor
+	 *
+	 * @param id the id of the new contact
+	 * @param name the name of the new contact
+	 */
 	public ContactImpl(int id, String name) {
 		if (name == null) {
 			throw new NullPointerException();
@@ -11,6 +17,7 @@ public class ContactImpl implements Contact {
 		}
 	}
 
+	@Override
 	public int getId() {
 		return id;
 	}
