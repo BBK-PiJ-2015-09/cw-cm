e4ef9afcf6d95951b03158fe313b3a29d622c17e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-05 19:45:52 +0000

    Throw IllegalArgumentException if Contact id is less than zero

--- ContactImpl.java
+++ ContactImpl.java
@@ -2,7 +2,7 @@ public class ContactImpl implements Contact {
 	private int id;
 
 	public ContactImpl(int id, String name) {
-		if (id == 0) {
+		if (id <= 0) {
 			throw new IllegalArgumentException();
 		} else {
 			this.id = id;
