0aea3910b7e2fa05fcf510c1a2befffa573698ed
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-05 19:53:15 +0000

    Remove shared code in ContactTest that is only used in one place so far (YAGNI principle)

--- ContactTest.java
+++ ContactTest.java
@@ -2,15 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactTest {
-	Contact contact;
-
-	@Before
-	public void buildUp() {
-		contact = new ContactImpl(1, "John Smith");
-	}
 
 	@Test
 	public void testsGetId() {
+		Contact contact = new ContactImpl(1, "John Smith");
 		int output = contact.getId();
 		int expected = 1;
 		assertEquals(expected, output);
