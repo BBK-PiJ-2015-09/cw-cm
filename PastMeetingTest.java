93b7dd5d244fc8ef184c275de76f24aa5d58108a
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-13 08:21:54 +0000

    Add PastMeetingTest

---
+++ PastMeetingTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class PastMeetingTest {
+
+	@Test
+	public void testsConstructor() {
+		Calendar date = Calendar.getInstance();
+		Set<Contact> contacts = new HashSet<Contact>();
+		contacts.add(new ContactImpl(1, "Jon"));
+		try {
+			new PastMeetingImpl(1, date, contacts);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
