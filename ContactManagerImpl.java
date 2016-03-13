d5694a332c2e1ce1976c528260c6b099da0357b3
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-13 23:28:19 +0000

    Make getContacts return contacts with names containing search term as a substring

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -92,7 +92,7 @@ public class ContactManagerImpl implements ContactManager {
 		} else {
 			Set<Contact> contactSet = new HashSet<Contact>();
 			for (Contact contact : contacts) {
-				if (contact.getName() == name) {
+				if (contact.getName().contains(name)) {
 					contactSet.add(contact);
 				}
 			}
