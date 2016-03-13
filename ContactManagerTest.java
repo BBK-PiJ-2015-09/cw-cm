e289813a986c0d275eb2880539dbb78c331e9a99
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-13 19:51:46 +0000

    Add addFutureMeeting test

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,7 +1,21 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
 
 public class ContactManagerTest {
+	Calendar date;
+	Set<Contact> contacts;
+	ContactManager manager;
+
+    @Before
+	public void setup() {
+		date = Calendar.getInstance();
+		contacts = new HashSet<Contact>();
+		contacts.add(new ContactImpl(1, "Jon"));
+		manager = new ContactManagerImpl();
+    }
 
 	@Test
 	public void testsConstructor() {
@@ -10,6 +24,13 @@ public class ContactManagerTest {
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
+	}
+
+	@Test
+	public void testsAddFutureMeeting() {
+		int output = manager.addFutureMeeting(contacts, date);
+		int expected = 0;
+		assertEquals(expected, output);
 	}
 
 }
