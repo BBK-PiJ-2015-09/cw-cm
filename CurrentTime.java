c438cb9c6180305dd1d6a49109c92e02def2faf1
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-14 18:42:26 +0000

    Add CurrentTime interface

---
+++ CurrentTime.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+
+/**
+ * A class to return the current time.
+ */
+public interface CurrentTime {
+
+	/**
+	 * Return the current time.
+	 *
+	 * @return a calendar set to the current time
+	 */
+	Calendar now();
+}
