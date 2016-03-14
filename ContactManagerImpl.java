cf7fa28e86dd1154e1c1b9a2a8f4908a12a0281d
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-14 20:51:29 +0000

    Throw NullPointerException from getPastMeetingListFor if contact is null

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -118,7 +118,9 @@ public class ContactManagerImpl implements ContactManager {
 
 	@Override
 	public List<PastMeeting> getPastMeetingListFor(Contact contact) {
-		if (!this.contacts.contains(contact)) {
+		if (contact == null) {
+			throw new NullPointerException();
+		} else if (!this.contacts.contains(contact)) {
 			throw new IllegalArgumentException();
 		} else {
 			List<PastMeeting> meetings = new ArrayList<PastMeeting>();
