cc26a1d6003d43d9f6fdeedd656ce054df6b47e8
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-14 18:48:11 +0000

    Add now test to CurrentTimeTest

--- CurrentTimeTest.java
+++ CurrentTimeTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Calendar;
 
 public class CurrentTimeTest {
 
@@ -10,6 +11,14 @@ public class CurrentTimeTest {
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
+	}
+
+	@Test
+	public void testsNow() {
+		CurrentTime currentTime = new CurrentTimeImpl();
+		Calendar output = currentTime.now();
+		Calendar expected = Calendar.getInstance();
+		assertEquals(expected, output);
 	}
 
 }
