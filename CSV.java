306c2743eadc3f1c22382d387cf73f33a9d52b9e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-14 23:19:20 +0000

    write pastMeetings to CSV

--- CSV.java
+++ CSV.java
@@ -7,6 +7,10 @@ import java.util.Set;
 public class CSV {
 
 	public static void save(List<PastMeeting> pastMeetings, List<FutureMeeting> futureMeetings, Set<Contact> contacts) {
+		writePastMeetings(pastMeetings);
+	}
+
+	private static void writePastMeetings(List<PastMeeting> pastMeetings) {
 		File output = new File("pastMeetings.csv");
 		PrintWriter writer = null;
 		try {
@@ -20,12 +24,32 @@ public class CSV {
 			stringBuilder.append(",");
 			stringBuilder.append("notes");
 			stringBuilder.append("\n");
+
+			for (PastMeeting meeting : pastMeetings) {
+				stringBuilder.append(meeting.getId());
+				stringBuilder.append(",");
+				stringBuilder.append(meeting.getDate().getTime());
+				stringBuilder.append(",");
+				stringBuilder.append(getContactIds(meeting.getContacts()));
+				stringBuilder.append(",");
+				stringBuilder.append(meeting.getNotes());
+				stringBuilder.append("\n");
+			}
+
 			writer.write(stringBuilder.toString());
 		} catch (FileNotFoundException ex) { //
 			System.out.println("Write to " + output + " failed.");
 		} finally {
 			writer.close();
 		}
+	}
+
+	private static String getContactIds(Set<Contact> contacts) {
+		String ids = "";
+		for (Contact contact : contacts) {
+			ids = ids + contact.getId() + ";";
+		}
+		return ids;
 	}
 
 }
